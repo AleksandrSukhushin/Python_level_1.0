89ca822571358e1a2dff6ce5697344ee582b59bd
Alexander <asukhushin@icloud.com>
2022-12-18 15:04:35 +0300

    Урок 2 - Лекция 2, Семинары 3-4, ДЗ 3-4

--- Lesson 2/Homework 3/Task 1.py
+++ Lesson 2/Homework 3/Task 1.py
@@ -2,3 +2,17 @@
 # стоящих на позиции с нечетным индексом.
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+
+length = int(input('Введите длину списка: '))
+my_list = []
+summa = 0
+
+for i in range(length):
+    number = random.randint(0,10)
+    my_list.append(int(number))
+    if i % 2 != 0:
+        summa += my_list[i]
+
+print(f'Сумма элементов списка: {my_list}, стоящих на позиции с нечетным индексом, равна: {summa}')
