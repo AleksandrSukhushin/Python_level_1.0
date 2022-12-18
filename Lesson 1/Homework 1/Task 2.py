009c142525542c42eb24c05753ca272ee7d0f7c4
Alexander <asukhushin@icloud.com>
2022-12-18 13:25:04 +0300

    Домашнее задание №1,2 с разборами

--- Lesson 1/Homework 1/Task 2.py
+++ Lesson 1/Homework 1/Task 2.py
@@ -1,12 +1,20 @@
 # Задача 2. Напишите программу для проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-X = int(input('Введите значение X: '))
-Y = int(input('Введите значение Y: '))
-Z = int(input('Введите значение Z: '))
+# Мое решение:
+# X = int(input('Введите значение X: '))
+# Y = int(input('Введите значение Y: '))
+# Z = int(input('Введите значение Z: '))
+# for X in range(2):
+#     for Y in range(2):
+#         for Z in range(2):
+#             print(not (X or Y or Z) == (not X and not Y and not Z))
+#             print(X, Y, Z)
 
-for X in range(2):
-    for Y in range(2):
-        for Z in range(2):
-            print(not (X or Y or Z) == (not X and not Y and not Z))
-            print(X, Y, Z)
+# Разбор задания с преподавателем:
+flag = True
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            expression = not(x or y or z) == (not x and not y and not z)
+            print(f'{x}\t{y}\t{z}\t{expression}')
--- Lesson 1/Homework 1/Task 5.py
+++ Lesson 1/Homework 1/Task 5.py
@@ -4,10 +4,18 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
-aX = int(input('Введите значение aX: '))
-aY = int(input('Введите значение aY: '))
-bX = int(input('Введите значение bX: '))
-bY = int(input('Введите значение bY: '))
+# Мое решение:
+# aX = int(input('Введите значение aX: '))
+# aY = int(input('Введите значение aY: '))
+# bX = int(input('Введите значение bX: '))
+# bY = int(input('Введите значение bY: '))
+# distance = round((((aX - bX)**2) + ((aY - bY)**2))**0.5, 1)
+# print(f'Раастояние между точками A {aX,aY} и B {bX,bY} равно {distance}')
 
-distance = round((((aX - bX)**2) + ((aY - bY)**2))**0.5, 1)
-print(f'Раастояние между точками A {aX,aY} и B {bX,bY} равно {distance}')
+# Разбор задания с преподавателем:
+import math
+first = input('Введите координаты точки А через пробел: ').split(' ')
+second = input('Введите координаты точки B через пробел: ').split()
+distance = (float(first[0]) - float(second[0]))**2 + (float(first[1]) - float(second[1]))**2
+print(f'Расстояние между точками = {round(math.sqrt(distance), 2)}')
+# print(f'Расстояние между точками = {round((distance**0.5), 2)}')
