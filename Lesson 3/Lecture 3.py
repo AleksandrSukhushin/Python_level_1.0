3a7946b9d30a4308784cee7e634ed18533cd6116
Alexander <asukhushin@icloud.com>
2023-01-13 19:43:57 +0300

    Урок 3 - Лекция 3, Семинары 5-6, ДЗ 5-6

--- Lesson 3/Lecture 3.py
+++ Lesson 3/Lecture 3.py
@@ -74,3 +74,32 @@
 # users = ['user1', 'user2', 'user3', 'user4', 'user5']
 # data = list(enumerate(users))
 # print(data)
+
+# from time import *
+# iter = 33
+# n = []
+# n_a = 0
+# n_b = 0
+# t = perf_counter() # time()
+# for i in range(1, iter):
+#     for j in range(iter):
+#         for k in range(iter):
+#             for l in range(iter):
+#                 n_a = i ** 3 + j ** 3
+#                 n_b = k ** 3 + l ** 3
+#                 if n_a == n_b and i != k and j != l and i != l and j != k:
+#                     if not n_a in n:
+#                         n.append(n_a)
+#                         # print(n_a, i, j, k, l)
+# n.sort()
+# print(n, sep='\n')
+# # print(time() - t)
+# print(perf_counter() - t)
+
+
+# for a in range(1, 33):
+#     for b in range(33):
+#         for c in range(33):
+#             for d in range(33):
+#                 if a ** 3 + b ** 3 == c ** 3 + d ** 3 and a != b and a != c and a != d and b != c and b != d and c != d:
+#                     print(a ** 3 + b ** 3)
